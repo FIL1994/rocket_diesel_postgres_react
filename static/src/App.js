08f2e11925c366c285ee298a7a7f4f67e0e31ce1
Philip Van Raalte <phil.vanraalte@gmail.com>
2018-04-16 11:16:07 -0400

    implemented ag-grid

--- static/src/App.js
+++ static/src/App.js
@@ -1,12 +1,22 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import { Container } from "semantic-ui-react";
+import axios from "axios";
+import _ from "lodash";
+import { AgGridColumn, AgGridReact } from "ag-grid-react";
+
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div>
-        App
-      </div>
+      <Container>
+        <div
+          className="ag-theme-material"
+          style={{ height: 650, width: "100%", marginTop: 15 }}
+        >
+          <AgGridReact />
+        </div>
+      </Container>
     );
   }
 }
--- static/src/index.js
+++ static/src/index.js
@@ -3,4 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import "ag-grid/dist/styles/ag-grid.css";
+import "ag-grid/dist/styles/ag-theme-material.css";
+
 ReactDOM.render(<App />, document.getElementById('root'));
