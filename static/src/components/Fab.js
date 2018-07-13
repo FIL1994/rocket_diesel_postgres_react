fac37cda103fb8d19453006bc11d7dd7366aac49
Philip Van Raalte <phil.vanraalte@gmail.com>
2018-07-13 00:14:45 +0000

    add mini prop

--- static/src/components/Fab.js
+++ static/src/components/Fab.js
@@ -2,13 +2,21 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const Fab = props => (
-  <button className="mdc-fab app-fab--absolute" aria-label="Favorite">
+  <button
+    className={`mdc-fab app-fab--absolute ${props.mini && "mdc-fab--mini"}`}
+    aria-label="Favorite"
+  >
     <span className="mdc-fab__icon material-icons">{props.icon}</span>
   </button>
 );
 
+Fab.defaultProps = {
+  mini: false
+};
+
 Fab.propTypes = {
-  icon: PropTypes.string.isRequired
+  icon: PropTypes.string.isRequired,
+  mini: PropTypes.bool
 };
 
 export default Fab;
